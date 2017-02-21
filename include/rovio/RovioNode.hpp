edb1481b83b661ebd4898879e70e2c8ebc458160
Nhan Pham <phamhuunhan0407@gmail.com>
2017-02-21 02:41:47 -0800

    rovio with Zed Camera and VN100 IMU

--- include/rovio/RovioNode.hpp
+++ include/rovio/RovioNode.hpp
@@ -197,8 +197,8 @@ class RovioNode{
 
     // Subscribe topics
     subImu_ = nh_.subscribe("imu0", 1000, &RovioNode::imuCallback,this);
-    subImg0_ = nh_.subscribe("cam0/image_raw", 1000, &RovioNode::imgCallback0,this);
-    subImg1_ = nh_.subscribe("cam1/image_raw", 1000, &RovioNode::imgCallback1,this);
+    subImg0_ = nh_.subscribe("zed/left/image_raw", 1000, &RovioNode::imgCallback0,this);
+    subImg1_ = nh_.subscribe("zed/right/image_raw", 1000, &RovioNode::imgCallback1,this);
     subGroundtruth_ = nh_.subscribe("pose", 1000, &RovioNode::groundtruthCallback,this);
     subGroundtruthOdometry_ = nh_.subscribe("odometry", 1000, &RovioNode::groundtruthOdometryCallback, this);
 
